30510e669736ad230f3c2324607074bbcca3fdd4
Joseph Oladunmoye <oladunmo@sheridancollege.ca>
2024-04-25 11:08:53 -0400

    End of Section 5

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -7,13 +7,12 @@ using Core.Interfaces;
 using Core.Specifications;
 using API.Dtos;
 using AutoMapper;
+using API.Errors;
 
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]  
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseAPIController
     {
        
         private readonly IGenericRepository<ProductType> _productTypeRepo;
@@ -42,11 +41,15 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ProductsToReturnDto>> GetProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
 
             var product = await _productsRepo.GetEntityWithSpec(spec);
+
+            if (product == null) return NotFound(new ApiResponse(404));
 
             return _mapper.Map<Product, ProductsToReturnDto>(product);
         }
